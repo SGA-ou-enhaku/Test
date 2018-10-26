8d01b9399aecde60ed05da26286d0c314f342377
WANG YUANBO <Yuanbo.Wang@sony.com>
2018-10-26 16:19:39 +0900

    Initial Commit

---
+++ Test10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test10
+//
+//  Created by ou on 2018/10/26.
+//  Copyright © 2018年 ou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test10/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Test10
+//
+//  Created by ou on 2018/10/26.
+//  Copyright © 2018年 ou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
