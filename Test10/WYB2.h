ff0474ff15bf3d7bcac6f5220c926d7f82f9529a
WANG YUANBO <Yuanbo.Wang@sony.com>
2018-12-25 11:29:22 +0900

    Wyb2

---
+++ Test10/WYB2.h
@@ -0,0 +1,17 @@
+//
+//  WYB2.h
+//  Test10
+//
+//  Created by ou on 12/25/18.
+//  Copyright © 2018 ou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WYB2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test10/WYB2.m
@@ -0,0 +1,13 @@
+//
+//  WYB2.m
+//  Test10
+//
+//  Created by ou on 12/25/18.
+//  Copyright © 2018 ou. All rights reserved.
+//
+
+#import "WYB2.h"
+
+@implementation WYB2
+
+@end
