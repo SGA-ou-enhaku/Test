c2316b81a76d257b95ccddd5be833358ae2cc01a
WANG YUANBO <Yuanbo.Wang@sony.com>
2018-12-25 11:30:34 +0900

    Maomao2

---
+++ Test10/Maomao2.h
@@ -0,0 +1,17 @@
+//
+//  Maomao2.h
+//  Test10
+//
+//  Created by ou on 12/25/18.
+//  Copyright © 2018 ou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Maomao2 : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
